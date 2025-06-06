fa258fb06585deb301e95913c8bd6b3d383f930d
zielisgabriel <josegabrielas0586@gmail.com>
2025-06-06 09:50:49 -0300

    raw: The server port was changed to 3103

--- src/server.py
+++ src/server.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-CORS(app, origins=['http://localhost:3000'], methods=["GET", "POST", "DELETE", "PUT"])
+CORS(app, origins=['http://localhost:3003'], methods=["GET", "POST", "DELETE", "PUT"])
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///crossx.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
